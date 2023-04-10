d932e0209eab1fb0c3f928a5605773c171d5ea36
Thiago Meira <thiagoluka@hotmail.com>
2023-04-10 00:05:36 -0300

    ItemsSteam migration (final part): * Moving old data into the new schema and cleaning up

--- db/migration_item_steam.sql
+++ db/migration_item_steam.sql
@@ -63,3 +63,94 @@ CREATE TABLE IF NOT EXISTS item_steam_assets
 	,removed_at TIMESTAMP
 	,CONSTRAINT unique_asset UNIQUE(user_id, asset_id)
 );
+
+
+-- the next two queries insert item definitions that went through the old inventory
+WITH old_descript_data AS (
+	SELECT * FROM item_descriptions
+	WHERE
+		(game_id, url_name) NOT IN (
+			SELECT game_id, market_url_name
+			FROM items_steam
+		)
+)
+INSERT INTO items_steam (
+	game_id
+	, item_steam_type_id
+	, name
+	, market_url_name
+)
+SELECT
+	game_id
+	, type_id
+	, name
+	, url_name
+FROM old_descript_data;
+
+
+WITH old_descript_data AS (
+	SELECT *
+	FROM item_descriptions
+	WHERE
+		(class_id) NOT IN (
+			SELECT class_id
+			FROM item_steam_descriptions isd
+		)
+)
+INSERT INTO item_steam_descriptions (
+	item_steam_id
+	, class_id
+)
+SELECT
+	is2.id
+	, odd.class_id
+FROM old_descript_data odd, items_steam is2
+WHERE
+	is2.game_id = odd.game_id
+	AND is2.market_url_name = odd.url_name;
+
+
+-- fixing created_at data at the new table
+WITH old_created_at_data AS (
+	SELECT
+		new_a.id AS id
+		, old_a.created_at AS old_created
+		, new_a.created_at AS new_created
+	FROM item_assets old_a
+	FULL OUTER JOIN item_steam_assets new_a
+		ON old_a.user_id = new_a.user_id AND old_a.asset_id = new_a.asset_id
+	WHERE old_a.created_at IS NOT NULL
+)
+UPDATE item_steam_assets isa
+SET created_at = old_c.old_created
+FROM old_created_at_data old_c
+WHERE isa.id = old_c.id;
+
+
+-- insert old assets data into the new table
+WITH old_assets_data AS (
+	SELECT
+		is2.id AS item_steam_id
+		, old_a.user_id
+		, old_a.asset_id
+		, old_a.marketable
+		, old_a.created_at
+		, old_a.removed_at
+	FROM item_assets old_a
+	FULL OUTER JOIN item_steam_assets new_a
+		ON old_a.user_id = new_a.user_id AND old_a.asset_id = new_a.asset_id
+	INNER JOIN item_descriptions old_d
+		ON old_a.description_id = old_d.id
+	INNER JOIN items_steam is2
+		ON is2.game_id = old_d.game_id AND is2.market_url_name = old_d.url_name
+	WHERE new_a.id IS NULL
+)
+INSERT INTO item_steam_assets (
+	item_steam_id
+	, user_id
+	, asset_id
+	, marketable
+	, created_at
+	, removed_at
+)
+SELECT * FROM old_assets_data;
