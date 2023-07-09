f2db9354344829bfed2f48f8aeabe05bae8978cd
Thiago Meira <thiagoluka@hotmail.com>
2023-07-09 13:23:42 -0300

    Int within range allows out of range values after confirmation

--- user_interfaces/InputValidation.py
+++ user_interfaces/InputValidation.py
@@ -3,19 +3,27 @@
 class InputValidation:
 
     @staticmethod
-    def int_within_range(lesser, greater, text_to_show: str = '') -> int:
+    def int_within_range(lesser, greater, text_to_show: str = '', allow_out_of_range: bool = True) -> int:
         user_input = 0
         invalid = True
-        error_message = f'Please enter a value between {lesser} and {greater}.'
+        error_message = f'Please enter a numeric value between {lesser} and {greater}.'
+        allow_out_of_range_message = 'This value is out of the pre established range. ' \
+                                     'Are you sure you want to continue with it?'
         while invalid:
             try:
                 user_input = int(input(text_to_show))
                 if user_input not in range(lesser, greater + 1):
-                    print(f'Value out of range. {error_message}')
+                    if allow_out_of_range:
+                        if InputValidation.yes_or_no(allow_out_of_range_message):
+                            invalid = False
+                        else:
+                            print(error_message)
+                    else:
+                        print(f'Value out of range. {error_message}')
                 else:
                     invalid = False
             except ValueError:
-                print(f'Non numeric input. {error_message}')
+                print(f'Non numeric input. Please enter a numeric value between {lesser} and {greater}.')
         return user_input
 
     @staticmethod
