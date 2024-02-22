2e3cc985e49faae816826a54693fa201d0e2fb2d
Thiago Meira <thiagoluka@hotmail.com>
2024-02-22 16:48:15 -0300

    refactor: add game_id and conclusion to buy order table

--- data_models/command/BuyOrder.py
+++ data_models/command/BuyOrder.py
@@ -9,7 +9,7 @@ class BuyOrder(BasePersistenceModel, name='buy_order'):
         elif source == 'create_buy_order':
             self._load_standard(user_id=kwargs['user_id'], empty_buy_order=False)
         elif source == 'cancel_buy_order':
-            self._set_to_inactive(steam_buy_order_id=kwargs['steam_buy_order_id'])
+            self._set_to_inactive(steam_buy_order_id=kwargs['steam_buy_order_id'], conclusion='Canceled')
 
     @staticmethod
     def table_name(table_type: str) -> str:
@@ -24,13 +24,14 @@ class BuyOrder(BasePersistenceModel, name='buy_order'):
         query = self._empty_buy_order_query() if empty_buy_order else self._upsert_buy_order_query()
         self._db_execute(query=query)
 
-    def _set_to_inactive(self, steam_buy_order_id: str) -> None:
+    def _set_to_inactive(self, steam_buy_order_id: str, conclusion: str) -> None:
         public = self.table_name(table_type='public')
         query = f"""
         UPDATE {public} pbo
         SET
               active = False
             , qtd_current = 0
+            , conclusion = '{conclusion}'
             , updated_at = NOW()::TIMESTAMP
             , removed_at = NOW()::TIMESTAMP
         WHERE
@@ -48,6 +49,7 @@ class BuyOrder(BasePersistenceModel, name='buy_order'):
         SET
         	  active = False
         	, qtd_current = 0
+            , conclusion = 'Vanished'
         	, updated_at = s.created_at
         	, removed_at = s.created_at
         FROM {staging} s
@@ -63,38 +65,45 @@ class BuyOrder(BasePersistenceModel, name='buy_order'):
     def _upsert_buy_order_query(self) -> str:
         public = self.table_name(table_type='public')
         staging = self.table_name(table_type='staging')
+        item_table = self.models['steam_item'].table_name(table_type='public')
         return f"""
         BEGIN TRANSACTION;
         
         INSERT INTO {public} (
-              steam_buy_order_id
-            , user_id
-            , item_id
-            , active
-            , price
-            , qtd_start
-            , qtd_current
-            , created_at
-            , updated_at
-            , removed_at
+	          game_id 
+	        , item_id 
+	        , user_id 
+	        , steam_buy_order_id 
+	        , active 
+	        , price 
+	        , qtd_start 
+	        , qtd_current 
+	        , conclusion
+	        , created_at 
+	        , updated_at 
+	        , removed_at 
         )
         SELECT 
-        	steam_buy_order_id,
-        	user_id,
-        	item_id,
-        	True AS active,
-        	price,
-        	quantity AS qtd_start,
-        	quantity AS qtd_current,
-        	created_at AS created_at,
-        	created_at AS updated_at,
-        	NULL AS removed_at
+	          si.game_id 
+	        , s.item_id 
+	        , s.user_id 
+	        , s.steam_buy_order_id 
+	        , True AS active 
+	        , s.price 
+	        , s.quantity AS qtd_start 
+	        , s.quantity AS qtd_current 
+	        , 'Undefined' AS conclusion
+	        , s.created_at AS created_at 
+	        , s.created_at AS updated_at 
+	        , NULL AS removed_at 
         FROM {staging} s
+        INNER JOIN {item_table} si ON s.item_id = si.id
         ON CONFLICT (steam_buy_order_id)
         DO UPDATE
         SET
         	  active = True
         	, qtd_current = EXCLUDED.qtd_current
+        	, conclusion = EXCLUDED.conclusion
         	, updated_at = EXCLUDED.created_at
         	, removed_at = NULL
         ;
--- data_models/query/BuyOrdersRepository.py
+++ data_models/query/BuyOrdersRepository.py
@@ -6,12 +6,11 @@ class BuyOrdersRepository(BaseQueryRepository):
     @classmethod
     def get_current_buy_orders(cls, user_id: int) -> list[tuple]:
         buy_order = cls.query_tables(table_type='buy_order')
-        item = cls.query_tables(table_type='steam_item')
         user_game = cls.query_tables(table_type='user_game')
         query = f"""
         SELECT
               bo.id AS id
-            , is2.game_id
+            , bo.game_id
             , bo.item_id
             , bo.steam_buy_order_id
             , bo.price AS price
@@ -21,9 +20,8 @@ class BuyOrdersRepository(BaseQueryRepository):
             , bo.updated_at
             , bo.removed_at
         FROM {buy_order} bo
-        INNER JOIN {item} is2 ON is2.id = bo.item_id
         INNER JOIN {user_game} ugt ON
-            ugt.user_id = bo.user_id AND is2.game_id = ugt.game_id
+            bo.user_id = ugt.user_id AND bo.game_id = ugt.game_id
         WHERE active AND bo.user_id = '{user_id}';
         """
         result = cls._db_execute(query=query)
@@ -32,12 +30,11 @@ class BuyOrdersRepository(BaseQueryRepository):
     @classmethod
     def get_inactive_with_created_at_rank(cls, user_id: int) -> list[tuple]:
         buy_order = cls.query_tables(table_type='buy_order')
-        item = cls.query_tables(table_type='steam_item')
         user_game = cls.query_tables(table_type='user_game')
         query = f"""
         SELECT
               bo.id AS id
-            , is2.game_id
+            , bo.game_id
             , bo.item_id
             , bo.steam_buy_order_id
             , bo.price
@@ -48,9 +45,8 @@ class BuyOrdersRepository(BaseQueryRepository):
             , bo.removed_at
             , RANK() OVER (PARTITION BY item_id ORDER BY created_at DESC) AS created_rank
         FROM {buy_order} bo
-        INNER JOIN {item} is2 ON is2.id = bo.item_id
         INNER JOIN {user_game} ugt ON
-            ugt.user_id = bo.user_id AND is2.game_id = ugt.game_id
+            bo.user_id = ugt.user_id AND bo.game_id = ugt.game_id
         WHERE NOT bo.active AND bo.user_id = '{user_id}';
         """
         result = cls._db_execute(query=query)
